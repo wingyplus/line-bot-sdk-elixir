0c30e66a43508fc6efb7f281721e71efcac16091
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2026-02-15 18:12:57 +0700

    Add webhook verify signature
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

---
+++ lib/line_bot_sdk/webhook.ex
@@ -0,0 +1,11 @@
+defmodule LINE.Bot.Webhook do
+  def signature_valid?(channel_secret, signature, body) do
+    case Base.decode64(signature) do
+      {:ok, decoded} ->
+        expected = :crypto.mac(:hmac, :sha256, channel_secret, body)
+        Plug.Crypto.secure_compare(expected, decoded)
+      :error ->
+        false
+    end
+  end
+end
